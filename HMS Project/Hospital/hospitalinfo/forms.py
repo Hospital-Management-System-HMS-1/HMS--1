4237013eea3a57bd261b5ba107ad574e827a6637
GadiReddemma5 <reddemmagadi5@gmail.com>
2025-10-10 14:53:29 +0530

    hopital form

--- HMS Project/Hospital/hospitalinfo/forms.py
+++ HMS Project/Hospital/hospitalinfo/forms.py
@@ -7,13 +7,3 @@ class HospitalForm(forms.ModelForm):
         fields = '__all__'   # include all fields from the model
         
         
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'city': forms.TextInput(attrs={'class': 'form-control'}),
-            'state': forms.TextInput(attrs={'class': 'form-control'}),
-            'contact_number': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'website': forms.URLInput(attrs={'class': 'form-control'}),
-            'rating': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.1'}),
-        }
