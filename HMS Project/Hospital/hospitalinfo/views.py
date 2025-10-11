6c2e4571d31ac687969b680ee02927081d1f3c6e
GadiReddemma5 <reddemmagadi5@gmail.com>
2025-10-11 18:02:51 +0530

    Update views.py

--- HMS Project/Hospital/hospitalinfo/views.py
+++ HMS Project/Hospital/hospitalinfo/views.py
@@ -36,7 +36,7 @@ def update_hospital(request,hospital_id):
 def delete_hospital(request,hospital_id):
     hospital=Hospital.objects.get(pk=hospital_id)
     hospital.delete()
-<<<<<<< HEAD
+
     return redirect('hospitalinfo:hospital_list')
 
 # Department views
@@ -71,8 +71,7 @@ def delete_depart(request,dept_id):
     depart=Department.objects.get(pk=dept_id)
     depart.delete()
     return redirect('hospitalinfo:depart_list')
-=======
-    return redirect(request,'hospitalinfo:hospital_list')
+
 
 
 def update_hospital(request, hospital_id):
@@ -85,4 +84,5 @@ def update_hospital(request, hospital_id):
     else:
         form = HospitalForm(instance=hospital)
     return render(request, 'update_hospital.html', {'form': form})
->>>>>>> 30982e75f4c9f975552e0f07d6aab6228eaa6b44
+
+
