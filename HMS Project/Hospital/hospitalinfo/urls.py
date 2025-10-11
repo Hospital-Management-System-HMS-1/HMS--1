c888685cf541c5786f16badad79aa3e062e7bb1f
GadiReddemma5 <reddemmagadi5@gmail.com>
2025-10-11 18:03:46 +0530

    Update urls.py

--- HMS Project/Hospital/hospitalinfo/urls.py
+++ HMS Project/Hospital/hospitalinfo/urls.py
@@ -6,6 +6,13 @@ app_name='hospitalinfo'
 urlpatterns = [
     # hospital urls
     path('hospital_list/',hospital_list,name='hospital_list'),
-    path('add/',add_hospital,name='add_hospital'),
-    path('delete/<int:hospital_id>/',delete_hospital,name='delete')
+    path('add_hospital/',add_hospital,name='add_hospital'),
+    path('delete_hospital/<int:hospital_id>/',delete_hospital,name='delete_hospital'),
+    path('update_hospital/<int:hospital_id>/',update_hospital,name='update_hospital'),
+    # department Urls
+    path('depart_list/',depart_list,name='depart_list'),
+    path('add_depart/',add_department,name='add_depart'),
+    path('update_depart/<int:dept_id>/',update_depart,name='update_depart'),
+    path('delete_depart/<int:dept_id>/',delete_depart,name='delete_depart')
+
 ]
