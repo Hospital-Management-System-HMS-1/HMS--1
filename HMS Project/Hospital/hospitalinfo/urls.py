236ee413575ae79570d3d8cba4c9142449eb5f02
hprathap <hprathap05@gmail.com>
2025-10-13 14:07:44 +0530

    done

--- HMS Project/Hospital/hospitalinfo/urls.py
+++ HMS Project/Hospital/hospitalinfo/urls.py
@@ -4,15 +4,15 @@ from .views import *
 app_name = 'hospitalinfo'
 
 urlpatterns = [
-    # hospital urls
-    path('hospital_list/',hospital_list,name='hospital_list'),
-    path('add_hospital/',add_hospital,name='add_hospital'),
-    path('delete_hospital/<int:hospital_id>/',delete_hospital,name='delete_hospital'),
-    path('update_hospital/<int:hospital_id>/',update_hospital,name='update_hospital'),
-    # department Urls
-    path('depart_list/',depart_list,name='depart_list'),
-    path('add_depart/',add_department,name='add_depart'),
-    path('update_depart/<int:dept_id>/',update_depart,name='update_depart'),
-    path('delete_depart/<int:dept_id>/',delete_depart,name='delete_depart'),
+    # Hospital URLs
+    path('hospital_list/', hospital_list, name='hospital_list'),
+    path('add_hospital/', add_hospital, name='add_hospital'),
+    path('update_hospital/<int:hospital_id>/', update_hospital, name='update_hospital'),
+    path('delete_hospital/<int:hospital_id>/', delete_hospital, name='delete_hospital'),
 
+    # Department URLs
+    path('depart_list/', depart_list, name='depart_list'),
+    path('add_depart/', add_department, name='add_depart'),
+    path('update_depart/<int:dept_id>/', update_depart, name='update_depart'),
+    path('delete_depart/<int:dept_id>/', delete_depart, name='delete_depart'),
 ]
