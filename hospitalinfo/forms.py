9106a09e0510ba74085a3fa3c9bcf948418f4932
Swetha082003 <swetha.akshay008@gmail.com>
2025-10-14 17:53:10 +0530

    Delete hospitalinfo directory

--- hospitalinfo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Hospital
-
-class HospitalForm(forms.ModelForm):
-    class Meta:
-        model = Hospital
-        fields = '__all__'   # include all fields from the model
-        
-        
