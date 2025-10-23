da0254e1864662800461e636fce6856e9158da6e
hprathap <hprathap05@gmail.com>
2025-10-23 16:58:31 +0530

    done

--- HMS Project/Hospital/hospitalinfo/urls.py
+++ HMS Project/Hospital/hospitalinfo/urls.py
@@ -4,7 +4,6 @@ from .views import *
 app_name = 'hospitalinfo'
 
 urlpatterns = [
-    # 
     path('hospital_list/', hospital_list, name='hospital_list'),
     path('add_hospital/', add_hospital, name='add_hospital'),
     path('update_hospital/<int:hospital_id>/', update_hospital, name='update_hospital'),
